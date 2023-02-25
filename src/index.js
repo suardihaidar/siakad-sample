63fa651f15fbe6f7310695500364de8da6ca5596
Muhammad Suardi <msuardi@binar.co.id>
2023-02-25 11:21:40 +0800

    feat: add base url

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "flowbite";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/siakad-sample">
       <App />
     </Router>
   </React.StrictMode>
