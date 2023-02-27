34d5bb996d56366522ca9bab1271d17d0ac3bc5c
Muhammad Suardi <msuardi@binar.co.id>
2023-02-27 12:19:30 +0800

    feat: add icons

--- src/assets/icons/icons.js
+++
@@ -1,5 +0,0 @@
-import { ReactComponent as IcPie } from "./ic-pie.svg";
-import { ReactComponent as IcPeople } from "./ic-people.svg";
-import { ReactComponent as IcExpandMore } from "./ic-expand-more.svg";
-
-export { IcPie, IcPeople, IcExpandMore };
