2844c484f9775ffa34dfe6717b8b9e7f40204641
Muhammad Suardi <msuardi@binar.co.id>
2023-03-02 12:46:15 +0800

    feat: krs page enhancement

--- src/pages/KRS.jsx
+++ src/pages/KRS.jsx
@@ -6,39 +6,47 @@ function KRS() {
     <div>
       <div className="uppercase text-gray-400 mb-3">KRS</div>
       <div className="flex my-5 font-semibold">
-        <div className="block w-full p-5 border rounded-lg dark:border-gray-700 text-center mr-5 bg-blue-50">
-          <p className="text-gray-400">Program Studi</p>
-          <span className="text-vintage-blue">Statistika</span>
+        <div className="block w-full p-5 border rounded-lg dark:border-gray-700 text-center mr-5">
+          <p className="text-gray-400 text-sm">Program Studi</p>
+          <span className="text-vintage-blue text-xl">Statistika</span>
         </div>
-        <div className="block w-full p-5 border rounded-lg dark:border-gray-700 text-center mr-5 bg-orange-50">
-          <p className="text-gray-400">Semester</p>
-          <span className="text-vintage-blue">Gasal 2022/2023</span>
+        <div className="block w-full p-5 border rounded-lg dark:border-gray-700 text-center mr-5">
+          <p className="text-gray-400 text-sm">Semester</p>
+          <span className="text-vintage-blue text-xl">Ganjil 2022/2023</span>
         </div>
-        <div className="block w-full p-5 border rounded-lg dark:border-gray-700 text-center mr-5 bg-green-50">
-          <p className="text-gray-400">SKS Maksimum</p>
-          <span className="text-vintage-blue">24</span>
+        <div className="block w-full p-5 border rounded-lg dark:border-gray-700 text-center mr-5">
+          <p className="text-gray-400 text-sm">SKS Maksimum</p>
+          <span className="text-vintage-blue text-xl">24</span>
         </div>
-        <div className="block w-full p-5 border rounded-lg dark:border-gray-700 text-center bg-red-50">
-          <p className="text-gray-400">Dosen PA/Wali</p>
-          <span className="text-vintage-blue">Robert Downey S.E M.Si</span>
+        <div className="block w-full p-5 border rounded-lg dark:border-gray-700 text-center">
+          <p className="text-gray-400 text-sm">Dosen PA/Wali</p>
+          <span className="text-vintage-blue text-xl">
+            Robert Downey S.E M.Si
+          </span>
         </div>
       </div>
 
       <div
-        className="p-4 my-5 font-medium text-white rounded-lg bg-orange-400 dark:bg-gray-800 dark:text-gray-300"
+        className="flex p-4 mb-4 text-sm text-orange-800 rounded-lg bg-orange-50 dark:bg-gray-800 dark:text-orange-300"
         role="alert"
       >
-        PERHATIAN! belum waktunya untuk giliran anda mengambil KRS,
-        <br />
-        Jadwal pengambilan KRS Anda mulai 2023-02-16 00:00:00 dan selesai
-        2023-02-24 23:59:59.
+        <Ic.InfoCircle />
+        <span className="sr-only">Info</span>
+        <div>
+          <span className="font-medium">
+            PERHATIAN! belum waktunya untuk giliran anda mengambil KRS
+          </span>
+          <br />
+          Jadwal pengambilan KRS Anda mulai 2023-02-16 00:00:00 dan selesai
+          2023-02-24 23:59:59.
+        </div>
       </div>
-
       <div
-        className="p-4 font-medium my-5 text-white rounded-lg bg-orange-400 dark:bg-gray-800 dark:text-gray-300"
+        className="flex p-4 mb-4 text-sm text-orange-800 rounded-lg bg-orange-50 dark:bg-gray-800 dark:text-orange-300"
         role="alert"
       >
-        Anda belum mengambil KRS
+        <Ic.InfoCircle />
+        <span className="font-medium">Anda belum mengambil KRS</span>
       </div>
 
       <div className="relative overflow-x-auto sm:rounded-lg">
@@ -123,23 +131,25 @@ function KRS() {
           type="button"
           data-modal-target="addKRSModal"
           data-modal-toggle="addKRSModal"
-          className="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3"
+          className="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-3 flex items-center"
         >
+          <Ic.Plus className="w-4 h-4 text-white mr-2" />
           Tambah
         </button>
         <button
           type="button"
           onClick={() => alert("selected MK is deleted!")}
-          className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3"
+          className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-3 flex items-center"
         >
+          <Ic.Bin className="w-4 h-4 text-white mr-2" />
           Hapus
         </button>
         <button
           type="button"
           onClick={() => alert("the KRS is printed!")}
-          className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex"
+          className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex items-center"
         >
-          <Ic.Printer className="w-5 h-5 text-white mr-3" />
+          <Ic.Printer className="w-4 h-4 text-white mr-2" />
           Cetak
         </button>
       </div>
